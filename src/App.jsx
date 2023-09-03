d33bda2c080d2c9b4c894f5447a028ef577cbd4b
Ulysses John Cunan <cunan.102469@angeles.sti.edu.ph>
2023-09-03 22:15:50 +0800

    test 2

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,10 @@ import GameComponent from './Components/GameComponent'
 import TuneIcon from '@mui/icons-material/Tune';
 
 export default function App() {
+
+  console.log("The secret is: ")
+  console.log(import.meta.env.VITE_SECRET)
+
   const [url, setUrl] = useState('https://free-to-play-games-database.p.rapidapi.com/api/games');
   const [gameData, setGameData] = useState([])
   const [platform, setPlatform] = useState("")
